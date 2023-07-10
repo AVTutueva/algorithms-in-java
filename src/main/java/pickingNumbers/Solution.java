6fe6057381b025b7651c8b2f7945a2b806604277
avtutueva <avtutueva@gmail.com>
2023-07-10 14:05:17 +0200

    added Picking Numbers solution

---
+++ src/main/java/pickingNumbers/Solution.java
@@ -0,0 +1,36 @@
+package pickingNumbers;
+
+import java.util.*;
+
+/*
+Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+Constraints:
+2 <= n <= 100
+0 < a[i] < 100
+ */
+public class Solution {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        int[] freq = new int[100];
+        for (int i: a){
+            freq[i] = freq[i] + 1;
+        }
+        int max = 0;
+        for (int i = 0; i < freq.length - 1; i++){
+            int temp = freq[i] + freq[i+1];
+            if (max < temp){
+                max = temp;
+            }
+        }
+        return max;
+    }
+
+}
+
